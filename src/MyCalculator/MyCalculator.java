18fc41d50121527d4649831847cc19176aa79abf
Alex <a.perekrestov@gmail.com>
2020-02-05 16:01:41 +0300

    Factorial

--- src/MyCalculator/MyCalculator.java
+++ src/MyCalculator/MyCalculator.java
@@ -5,6 +5,8 @@ import javax.swing.*;
 import javax.swing.text.DocumentFilter;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.text.AttributeSet;
@@ -13,13 +15,22 @@ import javax.swing.text.BadLocationException;
 
 public class MyCalculator extends JFrame {
     private JTextFile textField;
-    private JButton addition;
-    private JButton subtraction;
-    private JButton multiplication;
-    private JButton division;
+    private String doing = "+";
+
+    private JButton addition = new JButton("+");
+    private JButton subtraction = new JButton("-");
+    private JButton multiplication = new JButton("*");
+    private JButton division = new JButton("/");
+    private JButton factorial = new JButton("!");
+
+    private JButton result = new JButton("=");
+    private JButton clean = new JButton(("C"));
+
+    private int firstArg;
+    private int secondArg;
 
     public MyCalculator() {
-        super("Test ContentPane");
+        super("Simple Calculator");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         JPanel contents = new JPanel();
@@ -27,37 +38,219 @@ public class MyCalculator extends JFrame {
         textField.setHorizontalAlignment(JTextField.RIGHT);
         contents.add(textField);
 
-        JButton addition = new JButton("+");
         contents.add(addition);
-
-        contents.add(new JButton("-"));
-        contents.add(new JButton("*"));
-        contents.add(new JButton("/"));
-        contents.add(new JButton("!"));
-        contents.add(new JButton("="));
+        contents.add(subtraction);
+        contents.add(multiplication);
+        contents.add(division);
+        contents.add(factorial);
+        contents.add(clean);
+        contents.add(result);
 
         setContentPane(contents);
-        setSize(260, 130);
+        setSize(260, 170);
         setVisible(true);
 
+        KeyListener textFieldKeyListener = new KeyListener() {
+            String s = "";
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == 49 || e.getKeyCode() == 50 || e.getKeyCode() == 51 || e.getKeyCode() == 52
+                        || e.getKeyCode() == 53 || e.getKeyCode() == 54 || e.getKeyCode() == 55 || e.getKeyCode() == 56
+                        || e.getKeyCode() == 57 || e.getKeyCode() == 48) {
+
+                } else {
+                    s = textField.getText();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode() == 49 || e.getKeyCode() == 50 || e.getKeyCode() == 51 || e.getKeyCode() == 52
+                        || e.getKeyCode() == 53 || e.getKeyCode() == 54 || e.getKeyCode() == 55 || e.getKeyCode() == 56
+                        || e.getKeyCode() == 57 || e.getKeyCode() == 48) {
+
+                } else {
+                    textField.setText(s + "");
+                }
+            }
+        };
         ActionListener textFieldListener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println();
+                String localResult = "";
+
+                try {
+                    Integer.parseInt(textField.getText());
+                    secondArg = Integer.parseInt(textField.getText());
+                } catch (NumberFormatException e1) {
+                    secondArg = 0;
+                }
+
+                switch (doing) {
+                    case "+":
+                        localResult = "" + (firstArg + secondArg);
+                        break;
+                    case "-":
+                        localResult = "" + (firstArg - secondArg);
+                        break;
+                    case "*":
+                        localResult = "" + (firstArg * secondArg);
+                        break;
+                    case "/":
+                        if(secondArg != 0) {
+                            localResult = "" + (firstArg / secondArg);
+                        } else {
+                            localResult = "error int";
+                        }
+                        break;
+                }
+                textField.setText(localResult);
+                textField.requestFocus();
+                firstArg = 0;
+                secondArg = 0;
             }
         };
+        ActionListener cleanListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                firstArg = 0;
+                secondArg = 0;
+                textField.setText("");
+                textField.requestFocus();
+            }
+        };
+        ActionListener resultListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String localResult = "";
+
+                try {
+                    Integer.parseInt(textField.getText());
+                    secondArg = Integer.parseInt(textField.getText());
+                } catch (NumberFormatException e1) {
+                    secondArg = 0;
+                }
+
+                switch (doing) {
+                    case "+":
+                        localResult = "" + (firstArg + secondArg);
+                        break;
+                    case "-":
+                        localResult = "" + (firstArg - secondArg);
+                        break;
+                    case "*":
+                        localResult = "" + (firstArg * secondArg);
+                        break;
+                    case "/":
+                        if(secondArg != 0) {
+                            localResult = "" + (firstArg / secondArg);
+                        } else {
+                            localResult = "error int";
+                        }
+                        break;
+                }
+                textField.setText(localResult);
+                textField.requestFocus();
+                firstArg = 0;
+                secondArg = 0;
+            }
+        };
+
         ActionListener additionListener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println(addition.getText());
+                try {
+                    Integer.parseInt(textField.getText());
+                    firstArg = Integer.parseInt(textField.getText());
+                } catch (NumberFormatException e1) {
+                    firstArg = 0;
+                }
+                textField.setText("");
+                textField.requestFocus();
+                doing = addition.getText();
+            }
+        };
+        ActionListener subtractionListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Integer.parseInt(textField.getText());
+                    firstArg = Integer.parseInt(textField.getText());
+                } catch (NumberFormatException e1) {
+                    firstArg = 0;
+                }
+                textField.setText("");
+                textField.requestFocus();
+                doing = subtraction.getText();
+                System.out.println(doing);
+            }
+        };
+        ActionListener multiplicationListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Integer.parseInt(textField.getText());
+                    firstArg = Integer.parseInt(textField.getText());
+                } catch (NumberFormatException e1) {
+                    firstArg = 0;
+                }
+                textField.setText("");
+                textField.requestFocus();
+                doing = multiplication.getText();
+            }
+        };
+        ActionListener divisionListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Integer.parseInt(textField.getText());
+                    firstArg = Integer.parseInt(textField.getText());
+                } catch (NumberFormatException e1) {
+                    firstArg = 0;
+                }
+                textField.setText("");
+                textField.requestFocus();
+                doing = division.getText();
+            }
+        };
+        ActionListener factorialListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Integer.parseInt(textField.getText());
+                    firstArg = Integer.parseInt(textField.getText());
+                } catch (NumberFormatException e1) {
+                    firstArg = 0;
+                }
+
+                int f = firstArg;
+                for (int i = 1; i < firstArg; i++) {
+                    f = f * i;
+                    System.out.println(f);
+                }
+                textField.setText("" + f);
             }
         };
 
         addition.addActionListener(additionListener);
+        subtraction.addActionListener(subtractionListener);
+        multiplication.addActionListener(multiplicationListener);
+        division.addActionListener(divisionListener);
+        factorial.addActionListener(factorialListener);
+
+        clean.addActionListener(cleanListener);
+        result.addActionListener(resultListener);
         textField.addActionListener(textFieldListener);
+        textField.addKeyListener(textFieldKeyListener);
 
         // todo магия
-        ((AbstractDocument) textField.getDocument()).setDocumentFilter(new MyDocumentFilter());
+//        ((AbstractDocument) textField.getDocument()).setDocumentFilter(new MyDocumentFilter());
     }
 
     public static void main(String[] args) {
